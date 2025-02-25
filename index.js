566301bddfe028f071a43e8f9c028705e2423031
Dheeraj Pali <dheerajpali761@gmail.com>
2025-02-26 00:20:13 +0530

    changed port

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8000
 
 app.use(bodyParser.json());
-
+app.listen(port, () => console.log(`Server running on port ${port}`));
 // Basic root endpoint
 app.get('/', (req, res) => {
   res.send('Server is running!');
